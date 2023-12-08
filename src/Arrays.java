cc892ec26d58d736e1464c08c2c7f8a485a3165e
Elzida <152496247+Elzida@users.noreply.github.com>
2023-12-08 22:49:21 +0600

    Initial commit

---
+++ src/Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+    public static void main(String[] args){
+        int[] nums = new int[5];
+
+        nums[0] = 45;
+        nums[1] = 25;
+        nums[2] = 56;
+        nums[3] = 2;
+        nums[4] = 4;
+        int res = nums[0]+nums[4];
+        System.out.println(res);
+    }
+}
